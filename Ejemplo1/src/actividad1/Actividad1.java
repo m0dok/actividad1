c015a8cdf4575abb89a5378da591cd2301a08b75
Dani Gonzalez <danygonzest@gmail.com>
2023-11-20 14:44:48 +0100

    Updated Actividad1

--- Ejemplo1/src/actividad1/Actividad1.java
+++ Ejemplo1/src/actividad1/Actividad1.java
@@ -1,19 +1,27 @@
-/**
- * 
- */
 package actividad1;
+import java.util.Scanner;
 
-/**
- * 
- */
+//	Crea un programa anomenat FormattedDate amb una classe amb el mateix nom dins. El
+//	programa demanarà a l'usuari que introduïsca el dia, el mes i l'any del seu naixement (tots
+//	els valors són sencers). Llavors, es traurà per pantalla la data de naixement amb el següent
+//	format d/m/a. Per exemple, si l'usuari tecleja dia=7,mes=11, any=1990, el programa traurà
+//	7/11/1990.
+
 public class Actividad1 {
-
-	/**
-	 * @param args
-	 */
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Ingresa un dia: ");
+		int dia = sc.nextInt();
+		
+		System.out.print("Ingresa un mes: ");
+		int mes = sc.nextInt();
+		
+		System.out.print("Ingresa un año: ");
+		int año = sc.nextInt();
+		
+		System.out.printf("La fecha introducida es: %02d/%02d/%04d", dia, mes, año);
 	}
-
 }
---
+++ Ejemplo1/src/actividad1/Actividad2.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package actividad1;
+
+/**
+ * 
+ */
+public class Actividad2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
